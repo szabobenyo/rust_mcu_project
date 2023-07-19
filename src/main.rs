f6bd4a84ba8f6a9d3d586de57e152f898dc9597a
Bence SZABO <szabo.bence1103@gmail.com>
2023-07-19 14:50:45 +0200

    Get dependencies, implemented blinky example code

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,40 @@
 #![no_main]
 #![no_std]
 
-use panic_halt as _;
+extern crate cortex_m_rt;
+extern crate panic_halt;
+extern crate stm32l0;
 
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
+//use stm32l0::stm32l0x0;
+use stm32l0xx_hal::{pac, prelude::*, rcc::Config};
 
 #[entry]
 fn main() -> ! {
-    hprintln!("Hello, world!");
-    loop {}
+    hprintln!("LED lights are magic * * *");
+
+    let dp = pac::Peripherals::take().unwrap();
+    let cp = cortex_m::Peripherals::take().unwrap();
+
+    // Configure the clock.
+    let mut rcc = dp.RCC.freeze(Config::hsi16());
+
+    // Acquire the GPIOA peripheral. This also enables the clock for GPIOA in
+    // the RCC register.
+    let gpioa = dp.GPIOA.split(&mut rcc);
+
+    // Configure PA1 as output.
+    let mut led = gpioa.pa1.into_push_pull_output();
+
+    // Get the delay provider.
+    let mut delay = cp.SYST.delay(rcc.clocks);
+
+    loop {
+        led.set_high().unwrap();
+        delay.delay_ms(500_u16);
+
+        led.set_low().unwrap();
+        delay.delay_ms(500_u16);
+    }
 }
